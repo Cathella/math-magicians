64c9d3c6da07a837293efa377e49aa930bb07396
Cathella <ncathie214@gmail.com>
2021-11-26 21:07:50 +0300

    Remove linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
 const Calculator = () => {
-
-  const [data, setData] = useState ({
+  const [data, setData] = useState({
     total: null,
     next: null,
     operation: null,
@@ -11,9 +10,9 @@ const Calculator = () => {
 
   const buttonClick = (obj, value) => {
     const newResult = calculate(obj, value);
-    
+
     setData(newResult);
-  }
+  };
 
   return (
     <div className="container">
@@ -21,28 +20,28 @@ const Calculator = () => {
         { data.next || data.operation || data.total || 0 }
       </div>
       <div className="btn-container">
-          <button onClick={() => buttonClick(data, 'AC')} type="button">AC</button>
-          <button onClick={() => buttonClick(data, '+/-')} type="button">+/-</button>
-          <button onClick={() => buttonClick(data, '%')} type="button">%</button>
-          <button onClick={() => buttonClick(data, '÷')} className="action-btns" type="button">÷</button>
-          <button onClick={() => buttonClick(data, '7')} type="button">7</button>
-          <button onClick={() => buttonClick(data, '8')} type="button">8</button>
-          <button onClick={() => buttonClick(data, '9')} type="button">9</button>
-          <button onClick={() => buttonClick(data, 'x')} className="action-btns" type="button">x</button>
-          <button onClick={() => buttonClick(data, '4')} type="button">4</button>
-          <button onClick={() => buttonClick(data, '5')} type="button">5</button>
-          <button onClick={() => buttonClick(data, '6')} type="button">6</button>
-          <button onClick={() => buttonClick(data, '-')} className="action-btns" type="button">-</button>
-          <button onClick={() => buttonClick(data, '1')} type="button">1</button>
-          <button onClick={() => buttonClick(data, '2')} type="button">2</button>
-          <button onClick={() => buttonClick(data, '3')} type="button">3</button>
-          <button onClick={() => buttonClick(data, '+')} className="action-btns" type="button">+</button>
-          <button onClick={() => buttonClick(data, '0')} className="zero" type="button">0</button>
-          <button onClick={() => buttonClick(data, '.')} type="button">.</button>
-          <button onClick={() => buttonClick(data, '=')} className="action-btns" type="button">=</button>
+        <button onClick={() => buttonClick(data, 'AC')} type="button">AC</button>
+        <button onClick={() => buttonClick(data, '+/-')} type="button">+/-</button>
+        <button onClick={() => buttonClick(data, '%')} type="button">%</button>
+        <button onClick={() => buttonClick(data, '÷')} className="action-btns" type="button">÷</button>
+        <button onClick={() => buttonClick(data, '7')} type="button">7</button>
+        <button onClick={() => buttonClick(data, '8')} type="button">8</button>
+        <button onClick={() => buttonClick(data, '9')} type="button">9</button>
+        <button onClick={() => buttonClick(data, 'x')} className="action-btns" type="button">x</button>
+        <button onClick={() => buttonClick(data, '4')} type="button">4</button>
+        <button onClick={() => buttonClick(data, '5')} type="button">5</button>
+        <button onClick={() => buttonClick(data, '6')} type="button">6</button>
+        <button onClick={() => buttonClick(data, '-')} className="action-btns" type="button">-</button>
+        <button onClick={() => buttonClick(data, '1')} type="button">1</button>
+        <button onClick={() => buttonClick(data, '2')} type="button">2</button>
+        <button onClick={() => buttonClick(data, '3')} type="button">3</button>
+        <button onClick={() => buttonClick(data, '+')} className="action-btns" type="button">+</button>
+        <button onClick={() => buttonClick(data, '0')} className="zero" type="button">0</button>
+        <button onClick={() => buttonClick(data, '.')} type="button">.</button>
+        <button onClick={() => buttonClick(data, '=')} className="action-btns" type="button">=</button>
       </div>
     </div>
   );
-}
+};
 
 export default Calculator;
