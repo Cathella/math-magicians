ba9e5fd05b74b40635589d92eb1913e2b3757768
Cathella <ncathie214@gmail.com>
2021-12-01 15:45:16 +0300

    Linter errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,30 +2,31 @@ import React from 'react';
 import Nav from './Nav';
 import '../App.css';
 
-const Home = () => <>
-  <Nav />
-  <div className="home-wrapper">
-    <h2>Welcome to our page!</h2>
-    <div>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-        doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-        veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-        voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed
-        quia consequuntur magni dolores eos qui ratione voluptatem sequi
-        nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
-        sit amet, consectetur, adipisci velit, sed quia non numquam eius.
-      </p>
-      <p>
-        But I must explain to you how all this mistaken idea of denouncing pleasure and praising
-        pain was born and I will give you a complete account of the system, and expound the actual
-        teachings of the great explorer of the truth, the master-builder of human happiness.
-        No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those
-        who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.
-        Nor again is there anyone who loves or pursues or desires to obtain pain of itself.
-      </p>
+const Home = () => (
+  <>
+    <Nav />
+    <div className="home-wrapper">
+      <h2>Welcome to our page!</h2>
+      <div>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit
+          voluptatem accusantium doloremque laudantium, totam
+          rem aperiam, eaque ipsa quae ab illo inventore veritatis
+          et quasi architecto beatae vitae dicta sunt explicabo.
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+          aut odit aut fugit, sed
+        </p>
+        <p>
+          But I must explain to you how all this mistaken idea of denouncing
+          pleasure and praising pain was born and I will give you a
+          complete account of the system, and expound the actual teachings
+          of the great explorer of the truth, the master-builder of
+          human happiness. No one rejects, dislikes, or avoids pleasure
+          itself, because it is pleasure.
+        </p>
+      </div>
     </div>
-  </div>
-</>
+  </>
+)
 
 export default Home;
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Nav from './Nav';
 
-const Quote = () => <>
-  <Nav />
-  <div className="home-wrapper">
-    <h3 style={{
-      textAlign: 'center',
-      marginTop: '5em',
-      lineHeight: '1.8',
-    }}>
-      Mathematics is not about numbers, equations, computations, 
-      or algorithms: it is about understanding. --William Paul Thurston
-    </h3>
-  </div>
-</>
+const Quote = () => (
+  <>
+    <Nav />
+    <div className="home-wrapper">
+      <h3 style={{
+        textAlign: 'center',
+        marginTop: '5em',
+        lineHeight: '1.8',
+      }}>
+        Mathematics is not about numbers, equations, computations, 
+        or algorithms: it is about understanding. --William Paul Thurston
+      </h3>
+    </div>
+  </>
+)
 
 export default Quote;
